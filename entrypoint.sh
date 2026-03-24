568632e068beec8ec51de202159a371d43ee2625
KissMartin <kiss.martin2@students.jedlik.eu>
2026-03-24 09:14:46 +0100

    fix: define Gate for API documentation access and uncomment artisan commands in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 
-# php artisan config:clear
+php artisan config:clear
 php artisan config:cache
-# php artisan route:cache
+php artisan route:cache
 php artisan view:cache
 php artisan storage:link
 
